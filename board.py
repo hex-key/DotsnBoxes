c30b2e46c39dce0bb947f3f512d5146e7a748f3f
Elliot Lichtman <elliot.joseph.lichtman@gmail.com>
2022-10-14 10:46:37 -0700

    gameOver and getWinner

--- board.py
+++ board.py
@@ -54,7 +54,24 @@ class Board:
                     return False
         return True
 
-            
+    def getWinner(self):
+        if gameOver(self):
+            redCount = 0
+            blueCount = 0
+
+            for r in range(6):
+                for c in range(6):
+                    if board[r][c] == "R":
+                        redCount += 1
+                    else:
+                        blueCount += 1
+
+            if redCount > blueCount:
+                return "R"
+            else:
+                return "B"
+        else:
+            return None       
 
         
 

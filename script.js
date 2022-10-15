11161b6e71b8172395b9809695dfb8b02d74e48e
hexps13 <irischen0505@gmail.com>
2022-10-15 07:53:37 -0700

    export board function of dubious quality

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 function exportBoard() {
+    let anchors = document.getElementsByTagName('div');
+    let board = {
+        horizontalEdges : [],
+        verticalEdges : [],
+        squares: []
+    };
+
+    for (let i = 0; i < anchors.length; i++) {
+        let anchor = anchors[i];
+        if (anchor.className.includes("row-spacer")) { // rel info = horizontal lines, should be 6 of them
+            let arr = [];
+            for (child of anchor.children) {
+                if (child.className.includes("line-h-red")) {
+                    arr.push("R");
+                } else if (child.className.includes("line-h-blue")) {
+                    arr.push("B");
+                } else {
+                    arr.push(" ");
+                }
+                board.horizontalEdges.push(arr);
+            }
+        } else if (anchor.className.includes("row-squares")) {
+            let arrSquares = [];
+            let arrLines = [];
+
+            for (child of anchor.children) {
+                if (child.className.includes("square-red")) {
+                    arrSquares.push("R");
+                } else if (child.className.includes("square-blue")) {
+                    arrSquares.push("B");
+                } else if (child.className === "square") {
+                    arrSquares.push(" ");
+                } else if (child.className.includes("line-v-red")) {
+                    arrLines.push("R");
+                } else if (child.className.includes("line-v-blue")) {
+                    arrLines.push("B");
+                } else if (child.className === "line-v") {
+                    arrLines.push(" ");
+                } else {
+                    console.log("?????");
+                }
+
+                board.verticalEdges.push(arrLines);
+                board.squares.push(arrSquares);
+            }
+        }
+    }
+    return board;
 }
